eefd78243f162e10e6adea8b4ff2c8b7d6a9c3da
Basyras <basyrascz@gmail.com>
2022-12-17 22:22:44 +0100

    adding first action

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,39 @@
+using GlobExpressions;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 /// Support plugins are available for:
 ///   - JetBrains ReSharper        https://nuke.build/resharper
 ///   - JetBrains Rider            https://nuke.build/rider
 ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
 ///   - Microsoft VSCode           https://nuke.build/vscode
+
+[GitHubActions(
+    "continuous",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.Push },
+    InvokedTargets = new[] { nameof(NugetPush) })]
 class Build : NukeBuild
 {
 
+    [Parameter][Secret] readonly string NuGetApiKey;
 
     [GitRepository] readonly GitRepository Repository;
-    public static int Main() => Execute<Build>(x => x.Compile);
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    GitHubActions GitHubActions => GitHubActions.Instance;
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+    AbsolutePath OutputPackagesDirectory => OutputDirectory / "output" / "nugetPackages";
+
+    public static int Main() => Execute<Build>(x => x.NugetPush);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -20,12 +42,16 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(_ => _
+                .SetProject(Solution));
         });
 
     Target Restore => _ => _
         .Before(Compile)
         .Executes(() =>
         {
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
@@ -33,6 +59,41 @@ class Build : NukeBuild
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(_ => _
+                .EnableNoRestore()
+                .SetProjectFile(Solution));
         });
+
+    Target UnitTest => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .EnableNoRestore()
+                .CombineWith(
+                    Solution.GetProjects("*.UnitTests"),
+                    (settings, unitTestProject) => settings
+                        .SetProjectFile(unitTestProject)));
+        });
+
+    Target NugetPush => _ => _
+    .DependsOn(UnitTest)
+    .Executes(() =>
+    {
+        DotNetPack(_ => _
+            .DisableNoRestore()
+            .DisableNoBuild()
+            .SetProject(Solution)
+            .SetOutputDirectory(OutputPackagesDirectory));
+
+        var nugetPackages = OutputDirectory.GlobFiles("*.nupkg");
+
+        DotNetNuGetPush(_ => _
+            .SetSource("")
+            .SetApiKey(NuGetApiKey)
+            .CombineWith(nugetPackages, (_, nugetPackage) => _
+                .SetTargetPath(nugetPackage)));
+
+    });
 
 }
