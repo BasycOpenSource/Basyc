f668499dba57cfa59d0f59d68a80143cc5817c21
Basyras <basyrascz@gmail.com>
2022-12-24 16:28:40 +0100

    replacig spaces with tabs

--- build/Tasks/Git/Diff/SolutionChanges.cs
+++ build/Tasks/Git/Diff/SolutionChanges.cs
@@ -2,19 +2,19 @@
 
 public record SolutionChanges(string SolutionFullPath, bool IsSolutionChanged, FileChange[] SolutionItemsChanges, ProjectChanges[] ProjectChanges)
 {
-    public string[] GetChangedFilesFullPath()
-    {
-        //return ProjectChanges
-        //	.SelectMany(x => x.FileChanges)
-        //	.Select(x => x.FilePath)
-        //	.Concat(ProjectChanges.Where(x => x.IsProjectChanged).Select(x => x.ProjectFullPath))
-        //	.Concat(IsSolutionChanged ? new[] { SolutionFullPath } : Enumerable.Empty<string>())
-        //	.ToArray();
+	public string[] GetChangedFilesFullPath()
+	{
+		//return ProjectChanges
+		//	.SelectMany(x => x.FileChanges)
+		//	.Select(x => x.FilePath)
+		//	.Concat(ProjectChanges.Where(x => x.IsProjectChanged).Select(x => x.ProjectFullPath))
+		//	.Concat(IsSolutionChanged ? new[] { SolutionFullPath } : Enumerable.Empty<string>())
+		//	.ToArray();
 
-        return ProjectChanges
-            .SelectMany(x => x.GetChangedFilesFullPath())
-            .Concat(SolutionItemsChanges.Select(x => x.FullPath))
-            .Concat(IsSolutionChanged ? new[] { SolutionFullPath } : Enumerable.Empty<string>())
-            .ToArray();
-    }
+		return ProjectChanges
+			.SelectMany(x => x.GetChangedFilesFullPath())
+			.Concat(SolutionItemsChanges.Select(x => x.FullPath))
+			.Concat(IsSolutionChanged ? new[] { SolutionFullPath } : Enumerable.Empty<string>())
+			.ToArray();
+	}
 }
