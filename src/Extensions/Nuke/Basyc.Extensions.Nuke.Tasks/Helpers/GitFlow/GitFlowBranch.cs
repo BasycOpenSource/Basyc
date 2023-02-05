e8c50c12d4110ed3ff55cd0ef00837b0fa169f77
Nuke pipeline <dummy@email.com>
2023-02-05 12:09:50 +0100

    [Automated] Adding test history

---
+++ src/Extensions/Nuke/Basyc.Extensions.Nuke.Tasks/Helpers/GitFlow/GitFlowBranch.cs
@@ -0,0 +1,16 @@
+﻿namespace Basyc.Extensions.Nuke.Tasks.Helpers.GitFlow;
+
+public abstract record GitFlowBranch(string Name, GitFlowBranchType BranchType)
+{
+	public record Main() : GitFlowBranch("main", GitFlowBranchType.Main);
+
+	public record Hotfix(string Name) : GitFlowBranch(Name, GitFlowBranchType.HotFix);
+
+	public record Release(string Name) : GitFlowBranch(Name, GitFlowBranchType.Release);
+
+	public record Develop() : GitFlowBranch("develop", GitFlowBranchType.Develop);
+
+	public record Feature(string Name) : GitFlowBranch(Name, GitFlowBranchType.Feature);
+
+	public record PullRequest(string Name) : GitFlowBranch(Name, GitFlowBranchType.PullRequest);
+}
