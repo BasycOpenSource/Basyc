bf3e9cba97721111295710dd0be28668581503b7
Jan HADASCOK <jan.hadascok@cz.abb.com>
2023-05-15 22:28:56 +0200

    sync

--- src/Blazor/Basyc.Blazor.Controls/Tooltip/BasycTooltip.razor.cs
+++ src/Blazor/Basyc.Blazor.Controls/Tooltip/BasycTooltip.razor.cs
@@ -7,13 +7,13 @@ public partial class BasycTooltip
 {
     private readonly TooltipPartsMessenger messenger = new TooltipPartsMessenger();
 
-    [Parameter]
-    public RenderFragment? ChildContent { get; set; } = null;
+    [Parameter] public RenderFragment? ChildContent { get; set; } = null;
 
-    [Parameter]
-    public RenderFragment? TooltipContent { get; set; } = null;
+    [Parameter] public RenderFragment? TooltipContent { get; set; } = null;
 
     private string ChildContentId { get; init; } = Random.Shared.Next().ToString();
 
     protected override void OnInitialized() => base.OnInitialized();
+
+    protected override void OnParametersSet() => base.OnParametersSet();
 }
